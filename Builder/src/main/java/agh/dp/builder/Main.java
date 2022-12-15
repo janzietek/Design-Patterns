1fa6636300075a8e9f173b3ccd82277b646e6b09
janzietek <jzietek@student.agh.edu.pl>
2022-12-15 01:41:23 +0100

    Add json validation and some quick fixing

--- Builder/src/main/java/agh/dp/builder/Main.java
+++ Builder/src/main/java/agh/dp/builder/Main.java
@@ -5,6 +5,8 @@ import agh.dp.builder.Nodes.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Main {
     public static String getJsonContent(String filename) throws IOException {
@@ -12,20 +14,23 @@ public class Main {
     }
 
     public static boolean isValid(String json){
-        // TODO:
-        return true;
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.readTree(json);
+            return true;
+        } catch (IOException e) {
+            System.out.println("Provided json file is invalid.");
+            return false;
+        }
     }
 
     public static void main(String[] args) throws IOException {
         // insert path
         String jsonRaw = getJsonContent("src/main/resources/probe1.json");
-        if (!isValid(jsonRaw)) {
-            System.out.println("Provided json file is invalid");
-            return;
+        if (isValid(jsonRaw)) {
+            Manager manager = new Manager(jsonRaw);
+            JSONObject object = manager.createObjectBuilder().build();
+            System.out.println(object.toString());
         }
-
-        Manager manager = new Manager(jsonRaw);
-        JSONObject object = manager.createObjectBuilder().build();
-        System.out.println(object.toString());
     }
 }
